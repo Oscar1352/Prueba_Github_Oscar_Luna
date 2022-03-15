d049e4b8f453fd378bfdd3d3a4d822b65994abb6
Miguel Matul <miguemat4@gmail.com>
2022-03-15 09:23:49 -0600

    Programa en Python actualizado
    Se agregó un print con indicaciones sobre el avance de la evaluación

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ print("El programa consiste en escribir la nota obtenida en ésta programa\n")
 nota = float(input("Por favor ingresar la nota obtenida en éste programa:"))
 
 print("\nLa nota obtenida fue de, ",nota,", gracias por su colaboración :D")
+
+print("\nFelicidades, va muy bien con su evaluación parcial. Att. Ing. Miguel")
