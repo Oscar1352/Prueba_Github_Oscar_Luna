57d8dd933bccc54a4e42eaac18275b34b18b56a8
Oscar Luna <carolina.151270@gmail.com>
2022-03-15 09:32:28 -0600

    Actualización de comentarios archivo main
    Se agregaron comentarios para una mejor interpretación de lo sucedido en el programa.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # Declaración de variables a utilizar
 nota = 0
 
+# mensaje de bienvenida y explicación del programa
 print("Buenos días ingeniero")
 print("El programa consiste en escribir la nota obtenida en ésta programa\n")
 
 # Se le solicita al usuario ingresar su nota
 nota = float(input("Por favor ingresar la nota obtenida en éste programa:"))
 
+# Se imprime la nota ingresada
 print("\nLa nota obtenida fue de, ",nota,", gracias por su colaboración :D")
 
+# Comentario del docente
 print("\nFelicidades, va muy bien con su evaluación parcial. Att. Ing. Miguel")
 
+# Datos personales
 print("Nombre: Oscar Alejandro Luna Ordoñez       Carné: 1530622")
