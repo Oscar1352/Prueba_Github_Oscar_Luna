7c70f4aa61e71ff15a4de15aa2739570a053a4a0
Oscar Luna <carolina.151270@gmail.com>
2022-03-15 09:20:43 -0600

    Creación de un Archivo de lenguaje  Python
    Se creó un programa sencillo en el cual se le solicitará la nota al docente sobre el programa

---
+++ main.py
@@ -0,0 +1,10 @@
+# Declaración de variables a utilizar
+nota = 0
+
+print("Buenos días ingeniero")
+print("El programa consiste en escribir la nota obtenida en ésta programa\n")
+
+# Se le solicita al usuario ingresar su nota
+nota = float(input("Por favor ingresar la nota obtenida en éste programa:"))
+
+print("\nLa nota obtenida fue de, ",nota,", gracias por su colaboración :D")
